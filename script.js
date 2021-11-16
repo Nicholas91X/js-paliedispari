9476f29e572efc675bced917ce2a458c84718be3
Nicholas <romitinicholas@gmail.com>
2021-11-16 14:07:40 +0100

    con commenti

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const primaParola = prompt("Inserisci la prima parola");
-console.log(primaParola);
+console.log("La parola inserita è: " + primaParola);
 
 function palindroma(parola){
+//creo un array vuoto
 const arrayPrimaParola = [];
 
+//popolo il mio array vuoto con le lettere, in ordine contrario, della parola inserita dall'utente 
 for (i = parola.length - 1; i >= 0; i--) {
     arrayPrimaParola.push(parola[i]);
 }
-console.log(arrayPrimaParola);
+console.log("array con lettere inserite al contrario: " + arrayPrimaParola);
 
+//creo la variabile che conterrà la mia parola processata
 let parolaPalindroma = "";
+//riempio la mia variabile estraendo le lettere dal mio array
 for(i = 0; i < arrayPrimaParola.length; i++) {
     parolaPalindroma = parolaPalindroma +  arrayPrimaParola[i];
 }
 
-console.log(parolaPalindroma);
+console.log("Parola scritta al conrario " + parolaPalindroma);
 
+//confronto la parola inserita con la mia variabile
 if (parola == parolaPalindroma) {
     return "parola palindroma";
     
